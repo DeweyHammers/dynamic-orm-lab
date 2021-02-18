fb503cef1885aa299afd81127bdf3342e435fb29
Dewey <hammersdrichard@gmail.com>
2021-02-18 13:21:17 -0800

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,6 +5,7 @@ class InteractiveRecord
     def self.table_name
       self.to_s.downcase.pluralize
     end
+    
     def self.column_names
       sql = "pragma table_info('#{table_name}')"
       table_info = DB[:conn].execute(sql)
@@ -14,17 +15,21 @@ class InteractiveRecord
       end
       arr.compact
     end
+
     def initialize(options={})
       options.each do |property, value|
         self.send("#{property}=", value)
       end
     end
+
     def table_name_for_insert
       self.class.table_name
     end
+
     def col_names_for_insert
       self.class.column_names.delete_if {|col| col == "id"}.join(", ")
     end
+
     def values_for_insert
       values = []
       self.class.column_names.each do |col_name|
@@ -32,6 +37,7 @@ class InteractiveRecord
       end
       values.join(", ")
     end
+
     def save
       sql =
       <<-SQL
@@ -41,6 +47,7 @@ class InteractiveRecord
       DB[:conn].execute(sql)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
     end
+
     def self.find_by_name(name)
       sql =
       <<-SQL
@@ -50,7 +57,8 @@ class InteractiveRecord
       SQL
       DB[:conn].execute(sql, name)
     end
-      def self.find_by(attribute_hash)
+
+    def self.find_by(attribute_hash)
       value = attribute_hash.values.first
       formatted_value = value.class == Fixnum ? value : "'#{value}'"
       sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
